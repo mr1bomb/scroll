1eb9e4e7902c0cf8b95a44372d5a1b40bdbe9508
mr1bomb <13806700005@163.com>
2018-08-25 00:09:31 +0800

    new

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": 2015
+    },
+    "rules": {
+        //"no-console": 2,
+       // "no-alert":2
+    }
+};
--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 /**
  * Created by dell on 2018/8/23.
  */
-console.log(123);
+console.log(123123);
 alert(2);
 
